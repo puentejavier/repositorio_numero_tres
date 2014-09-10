517d9e884a9b9c3e61f998350bbef1bd5f44effd
puentejavier <puentejavier80@gmail.com>
2014-09-10 21:22:13 +0200

    guardo repositorio_tres

---
+++ js/noentiendojava.js
@@ -0,0 +1,44 @@
+/*//deinimos variable local
+var variableGlobal = 10;
+
+//creamos una funcion
+miFuncion = function(par, pare)
+{var variableLocal = 20 ;
+ var suma = variableGlobal + variableLocal + par;
+ var resultado = pare + suma};
+
+document.write( suma);
+
+console.log("variableGlobal" + " + " + "variableLocal")
+
+miFuncion(10, "la suma de las variables es igual a : ");*/
+
+
+/*// ejercicio 
+
+var resultado;
+var numUno = parseInt(prompt( "introduzca una cifra"));
+var numDos = parseInt(prompt( "introduzca una cifra"));
+
+function suma() {
+	resultado = numUno + numDos;
+	alert("el resultado es" + resultado);
+}
+
+suma();*/
+
+//arrays
+var resultado
+
+var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agisto","Septiembre","Octubre","Noviembre","Diciembre"];
+ //mes = ("03");
+
+/*function elijames () {
+	
+	resultado = meses + elija ;*/
+	
+	
+	alert (meses[03]);
+
+
+//elijames ();
---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*//vamos a intentar a hacer arrays
+
+var valores = ["0" ,"1" , "2" ,"3", "4"]
+
+var dias = ["lunes","martes","miercoles","jueves","viernes"]
+
+alert ("lunes[0]"  + "martes[1]" +  "miercoles[2]" + "jueves[3]" + "viernes[4]") ;/*/
+
+//boleanos
+
+var edad = falso
+var nombre = verdadero
+
+alert ("edad")
+
